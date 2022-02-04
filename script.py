14f1f4e393cb7ee031892da0002afccdaf718235
Livery Bee <94587553+LiveryBee@users.noreply.github.com>
2022-02-04 17:31:35 +0530

    Update script.py

--- script.py
+++ script.py
@@ -52,9 +52,9 @@ See <i>/help</i> for commands and more details.</b>
 """
 
 
-    ABOUT_MSG = """⭕️<b>My Name : TroJanz Filter Bot</b>
+    ABOUT_MSG = """⭕️<b>My Name : John Kattadi - The Filter Bot</b>
 
-⭕️<b>Creater :</b> @TroJanzHEX    
+⭕️<b>Creater :</b> @AchuzzzTalksOfficial    
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
